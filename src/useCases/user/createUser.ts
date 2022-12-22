eaca6d9465c7eeab3f6ba0f00076ba6353fd76d9
Wellington Nobre <wellington.multilaser@gmail.com>
2022-12-22 16:47:54 -0300

    Adicionando crypto

--- src/useCases/user/createUser.ts
+++ src/useCases/user/createUser.ts
@@ -1,6 +1,7 @@
 import { v4 } from 'uuid'
 import { PrismaClient, User } from '@prisma/client'
 
+import { crypt } from '@services/crypto'
 import { UserDto } from '@domain/dtos/user'
 
 const prisma = new PrismaClient()
@@ -10,12 +11,14 @@ export class CreateUsersUseCase {
     constructor() {}
 
     async handle(user: Omit<UserDto, 'id'>): Promise<User> {
+        const hashed = crypt(user.password)
         const createUser = await prisma.user.create({
             data: {
                 id: v4(),
                 name: user.name,
                 email: user.email,
-                password: user.password
+                password: hashed.hash,
+                salt: hashed.salt
             }
         })
         return createUser
